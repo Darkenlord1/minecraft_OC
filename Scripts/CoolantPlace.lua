f574d0f3401aa999b56729dd3cb0f18dd623ad8d
Danila Ilinsky <ilinsky.danila@yandex.ru>
2024-07-23 10:48:29 +0300

    Create CoolantPlace.lua

---
+++ Scripts/CoolantPlace.lua
@@ -0,0 +1,25 @@
+local robot = require("robot") 
+local computer = require("computer") 
+local component = require("component") 
+
+local me = component.upgrade_me
+local db = component.database
+local inventory = component.inventory_controller
+
+local coolantName = "360k NaK Coolantcell"
+
+function robot_error(msg)
+    print("Ошибка: ", msg)
+    computer.beep(1000,0.3)
+    computer.beep(1000,0.3)
+    computer.beep(1000,0.3)
+    os.exit()
+end
+
+function getCoolant()
+    me.requestItems(db.get(1), 1)
+end
+
+function putCoolant()
+
+end
